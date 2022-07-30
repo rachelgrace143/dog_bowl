a9d61684c280b3757944966d00d58f5050c386c1
rachelgrace143 <110014654+rachelgrace143@users.noreply.github.com>
2022-07-30 16:41:59 +1200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from machine import I2C
 from lcd_api import LcdApi
 from i2c_lcd import I2cLcd
 
-led = machine.Pin(17, machine.Pin.OUT)
-btn = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
+relay = machine.Pin(17, machine.Pin.OUT)
+switch = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
 
 last = time.ticks_ms()
 count = 0
@@ -15,19 +15,16 @@ I2C_ADDR     = 0x27
 I2C_NUM_ROWS = 2
 I2C_NUM_COLS = 16
 
-def button_handler(pin):
-    global last, btn, count
-      
-    if pin is btn:
-        if time.ticks_diff(time.ticks_ms(), last) > 1100:
-            led.on()
-            time.sleep(1)
+while True:
+    logic_state = switch.value()
+    if logic_state == False:
+        if time.ticks_diff(time.ticks_ms(), last) > 2100:
+            relay.on()
+            time.sleep(2)
             count = count + 1
             i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
             lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
             lcd.putstr("DOG BOWL REFILL COUNT: " + str(count))
-            last = time.ticks_ms()
-            led.off()
-    
-led.value(0)
-btn.irq(trigger = machine.Pin.IRQ_RISING, handler = button_handler)
+            last = time.ticks_ms()            
+    elif logic_state == True:
+        relay.off()
